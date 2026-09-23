f81987ddfd4081bcf107298b5090dfd087011e48
Websitetrying-33 <johnmondigorowel@gmail.com>
2026-09-23 10:07:25 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Mga middleware para makatanggap ng JSON at payagan ang HTML na kumonekta (CORS)
+// Mga middleware para makatanggap ng JSON, CORS, at i-serve ang static files (tulad ng index.html)
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname)));
 
 // Storage para sa pinakabagong natanggap na GCash SMS
 let latestPayment = null;
+
+// Route para i-load ang index.html kapag binuksan ang pangunahing URL
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 // Endpoint kung saan nagpapadala ng HTTP POST request ang MacroDroid
 app.post('/sms-webhook', (req, res) => {
